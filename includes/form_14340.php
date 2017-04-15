d532a92fc3821e9885848b366aa6535e41d95284
Andres Jimenez <promontorio.club@icloud.com>
2017-04-14 23:43:36 -0500

    gh-pages

--- includes/form_14340.php
+++ includes/form_14340.php
@@ -1,14 +1,17 @@
 <?php	
+	if(empty($_POST['']) && strlen($_POST['']) == 0)
+	{
+		return false;
+	}
 	
-	
-	$select_158 = $_POST['select_158'];
+	$ = $_POST[''];
 	
 	$to = 'receiver@yoursite.com'; // Email submissions are sent to this email
 
 	// Create email	
 	$email_subject = "Message from My Blocs Website.";
 	$email_body = "You have received a new message. \n\n".
-				  "Select_158: $select_158 \n";
+				  ": $ \n";
 	$headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\n";	
 	$headers .= "From: contact@yoursite.com\n";
 	$headers .= "Reply-To: DoNotReply@yoursite.com";	
