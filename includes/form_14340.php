ec77a202ff545630d4eae1b99574b01a6669eeca
Andres Jimenez <promontorio.club@icloud.com>
2017-04-14 23:20:30 -0500

    gh-pages

--- includes/form_14340.php
+++ includes/form_14340.php
@@ -1,18 +1,16 @@
 <?php	
 	
 	
-	$input_81 = $_POST['input_81'];
-	$input_877 = $_POST['input_877'];
-	$input_539 = $_POST['input_539'];
+	$select_158 = $_POST['select_158'];
 	
-	$to = 'promontorio.club@gmail.com'; // Email submissions are sent to this email
+	$to = 'receiver@yoursite.com'; // Email submissions are sent to this email
 
 	// Create email	
 	$email_subject = "Message from My Blocs Website.";
 	$email_body = "You have received a new message. \n\n".
-				  "Input_81: $input_81 \nInput_877: $input_877 \nInput_539: $input_539 \n";
+				  "Select_158: $select_158 \n";
 	$headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\n";	
-	$headers .= "From: promontorio.club@icloud.com\n";
+	$headers .= "From: contact@yoursite.com\n";
 	$headers .= "Reply-To: DoNotReply@yoursite.com";	
 	
 	mail($to,$email_subject,$email_body,$headers); // Post message
